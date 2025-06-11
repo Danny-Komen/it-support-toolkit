14814941153a041a697a46fc3efb4023855d2af4
Danny Komen <kipropd2@gmail.com>
2025-06-11 14:45:57 +0300

    Add some more tools; Clean cache, Create Backup, View System Logs

--- support_toolkit.sh
+++ support_toolkit.sh
@@ -4,15 +4,18 @@
 
 while true; do
   clear
-  echo "=============================="
-  echo "   IT SUPPORT TOOLKIT v1.0 by Danny"
-  echo "=============================="
-  echo "1. View System Information"
-  echo "2. Check Disk Usage"
-  echo "3. Test Internet Connectivity"
-  echo "4. Exit"
-  echo "------------------------------"
-  read -p "Choose an option [1-4]: " option
+  	echo "=============================="
+  	echo "    IT SUPPORT TOOLKIT v1.1"
+  	echo "=============================="
+    echo "1. View System Information"
+  	echo "2. Check Disk Usage"
+  	echo "3. Test Internet Connectivity"
+  	echo "4. Clean System Cache"
+  	echo "5. Create System Backup"
+  	echo "6. View System Logs"
+  	echo "7. Exit"
+ 	echo "------------------------------"
+  	read -p "Choose an option [1-7]: " option
 
   case $option in
     1)
@@ -35,9 +38,24 @@ while true; do
       read -p "Press enter to continue..."
       ;;
     4)
-      echo "👋 Exiting. Stay sharp!"
-      break
+      echo -e "\n🧹 Cleaning system cache..."
+      sudo rm -rf /Library/Caches/* ~/Library/Caches/*
+      echo "✅ Cache cleaned successfully for the permitted files."
+      read -p "Press enter to continue..."
       ;;
+    5)
+      echo -e "\n📦 Creating system backup..."
+      BACKUP_NAME="sys_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+      tar -czf $BACKUP_NAME /var/log /etc > /dev/null 2>&1
+      echo "✅ Backup saved as $BACKUP_NAME"
+      read -p "Press enter to continue..."
+      ;;
+    6)
+      echo -e "\n🕵️ Viewing latest system logs..."
+      sudo tail -n 100 /var/log/system.log 	#Displays the last 100 lines in the system logs.
+      read -p "Press enter to continue..."
+      ;;
+
     *)
       echo "Invalid option. Try again."
       sleep 1
